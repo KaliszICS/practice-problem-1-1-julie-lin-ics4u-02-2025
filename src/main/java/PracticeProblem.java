212e6abc1c73fa83007b0508272b895200e00f1f
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-03-05 04:26:12 +0000

    you made mistake in tests for q1!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,30 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int sum2D(int[][] arr){
+		int sum = 0; 
+		for (int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[i].length; j++){
+				sum += arr[i][j];
+			}
+		}
+		return sum; 
 	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
 		//Write question 2 code here
+	public static int sumRow (int[][] arr, int row ){
+		int sum = 0; 
+		for (int col = 0; col < arr[row].length; col++){
+			sum += arr[row][col];
+		}
+		return sum; 
 	}
 
-	public static void q3() {
 		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static int sumColumn (int[][] arr, int col ){
+		int sum = 0; 
+		for (int row = 0; row < arr.length; row++){
+			sum += arr[row][col];
+		}
+		return sum; 
 	}
 
 }
